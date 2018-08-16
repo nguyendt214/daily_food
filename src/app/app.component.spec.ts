bb6ceb728912bf0593bb48df23f6663a1e1a3e32
Aloulou Amine <amine.aloulou.ext@nordnet.fr>
2018-08-16 12:10:47 +0200

    MU-3 fix tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,15 +2,13 @@
 
 import { TestBed, async} from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { TranslateModule } from '@ngx-translate/core';
 
 import { CoreModule } from './core/core.module';
-import { LayoutModule } from './layout/layout.module';
 import { SharedModule } from './shared/shared.module';
 import { RoutesModule } from './routes/routes.module';
 import { APP_BASE_HREF } from '@angular/common';
 
-describe('App: Ng2angle', () => {
+describe('App: mu-angular-seed', () => {
     beforeEach(() => {
 
         jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
@@ -20,9 +18,7 @@ describe('App: Ng2angle', () => {
                 AppComponent
             ],
             imports: [
-                TranslateModule.forRoot(),
                 CoreModule,
-                LayoutModule,
                 SharedModule,
                 RoutesModule
             ],
