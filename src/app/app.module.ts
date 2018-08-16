67aac23a8dc0fb825f45280ff590774e5c5a8683
Aloulou Amine <amine.aloulou.ext@nordnet.fr>
2018-08-16 16:27:22 +0200

    MU-3 remove translate loader.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateService, TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { RoutesModule } from './routes/routes.module';
-
-// https://github.com/ocombe/ng2-translate/issues/218
-export function createTranslateLoader(http: HttpClient) {
-    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 @NgModule({
     declarations: [
@@ -25,14 +17,7 @@ export function createTranslateLoader(http: HttpClient) {
         BrowserAnimationsModule, // required for ng2-tag-input
         CoreModule,
         SharedModule.forRoot(),
-        RoutesModule,
-        TranslateModule.forRoot({
-            loader: {
-                provide: TranslateLoader,
-                useFactory: (createTranslateLoader),
-                deps: [HttpClient]
-            }
-        })
+        RoutesModule
     ],
     providers: [],
     bootstrap: [AppComponent]
