b2f9291ab309b7fdbb4c16b99e04e5f7a436a05a
Aloulou Amine <amine.aloulou.ext@nordnet.fr>
2018-10-26 17:18:30 +0200

    update private APIM conf.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,6 @@ export const environment = {
   sso_url: 'https://dev-503543.oktapreview.com/oauth2/default',
   sso_client_id: '0oagd06t2kthO8H6P0h7',
   sso_redirect_uri: 'https://mu.nordnet.fr/%%package_name%%/implicit/callback',
-  mu_api: 'https://apim.kong.dev.nordnet.fr:8443/mu-server-api/api',
-  mu_api_key: '74c138e5-a3aa-466d-a17e-c5bfc5fd945e',
+  mu_api: 'https://apim-priv.nordnet.fr/mu-server-api/api',
+  mu_api_key: 'ca3dd62a-40b6-4c2d-b69c-06ef172e0aa3',
 };
