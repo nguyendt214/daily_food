8f4d41d1868b1350083bfbfa317fa755e95ebe35
Loic Doignie <loic.doignie.ext@nordnet.fr>
2018-12-18 17:17:59 +0100

    update seed.@mu/common
    update environment.staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -4,8 +4,8 @@ export const environment = {
   version: '%%package_version%%',
   app_name: '%%package_name%%',
   sso_url: 'https://dev-503543.oktapreview.com/oauth2/default',
-  sso_client_id: '0oagcypmebe7Cvjoj0h7',
-  sso_redirect_uri: 'http://appnodejs1-c1.recette.nordnet.fr:4200/%%package_name%%/implicit/callback',
+  sso_client_id: '0oaif7b8d70Aq8C6h0h7',
+  sso_redirect_uri: 'http://mu.rec.nordnet.fr/%%package_name%%/implicit/callback',
   mu_api: 'https://apim.kong.dev.nordnet.fr:8443/mu-server-api/api',
   mu_api_key: '74c138e5-a3aa-466d-a17e-c5bfc5fd945e',
 };
