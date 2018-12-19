2535abbbf0dd4b0eef5ee7b71919c4ab65026f29
Loic Doignie <loic.doignie.ext@nordnet.fr>
2018-12-19 10:57:46 +0100

    update environment.staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -6,6 +6,6 @@ export const environment = {
   sso_url: 'https://dev-503543.oktapreview.com/oauth2/default',
   sso_client_id: '0oaif7b8d70Aq8C6h0h7',
   sso_redirect_uri: 'http://mu.rec.nordnet.fr/%%package_name%%/implicit/callback',
-  mu_api: 'https://apim.kong.dev.nordnet.fr:8443/mu-server-api/api',
+  mu_api: 'https://apim-kong-staging.dev.nordnet.fr:8443/mu-server-api/api',
   mu_api_key: '74c138e5-a3aa-466d-a17e-c5bfc5fd945e',
 };
