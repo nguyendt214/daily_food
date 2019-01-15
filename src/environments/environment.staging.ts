39a8f69b1cfa9fc0193a23c3f177ed2d5d8cd4c7
Loïc Doignié <loic.doignie@nordnet.com>
2019-01-15 16:30:47 +0100

    Update environment.staging.ts with mu.recette.nordnet.fr

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -5,7 +5,7 @@ export const environment = {
   app_name: '%%package_name%%',
   sso_url: 'https://dev-503543.oktapreview.com/oauth2/default',
   sso_client_id: '0oaif7b8d70Aq8C6h0h7',
-  sso_redirect_uri: 'http://mu.rec.nordnet.fr/%%package_name%%/implicit/callback',
+  sso_redirect_uri: 'http://mu.recette.nordnet.fr/%%package_name%%/implicit/callback',
   mu_api: 'https://apim-kong-staging.dev.nordnet.fr:8443/mu-server-api/api',
   mu_api_key: '74c138e5-a3aa-466d-a17e-c5bfc5fd945e',
 };
