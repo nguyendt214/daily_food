fe83a52d81a04e49365b60494eb9fcee386028d5
Kevin <nguyendt214@gmail.com>
2019-03-11 16:16:38 +0700

    Create report component

--- src/app/routes/report/report.module.ts
+++ src/app/routes/report/report.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ReportComponent } from './report/report.component';
 import { SharedModule } from '../../shared/shared.module';
+import { MainReportComponent } from './report.main.component';
 
 @NgModule({
-    declarations: [ReportComponent],
+    declarations: [
+      MainReportComponent,
+      ReportComponent
+    ],
     imports: [
       SharedModule
     ]
