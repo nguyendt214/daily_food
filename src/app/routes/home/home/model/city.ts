070cb3bed9c9df88a9c7138aeef33d2b6f30e15f
Kevin <nguyendt214@gmail.com>
2019-03-11 10:46:45 +0700

    Update for cities part + ENV

--- src/app/routes/home/home/model/city.ts
+++ src/app/routes/home/home/model/city.ts
@@ -1,5 +1,6 @@
 export interface ICity {
   cityCode?: string;
+  postCode?: string;
   city?: string;
   cityDisplay?: string;
   state?: number; // 0 OLD, 1 NEW
--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -7,7 +7,7 @@ export const environment = {
   sso_client_id: '0oagd6vz6u9mvfogi0h7',
   sso_redirect_uri: 'http://mu.local.nordnet.fr:4200/implicit/callback',
   mu_api: 'https://apim.kong.dev.nordnet.fr:8443/mu-server-api/api',
-  mu_api_key: '74c138e5-a3aa-466d-a17e-c5bfc5fd945e',
+  mu_api_key: 'f3bd8c7f-8713-4742-aec3-79ef4af9051a',
   direct_scout_api: 'https://scout.nordnet.test/api',
   kong_url: 'https://apim.kong.dev.nordnet.fr:8443/adp-api/',
   adp_api: 'graphql'
